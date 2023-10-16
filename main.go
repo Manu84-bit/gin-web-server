5c27e37fac90e28bca617b1662c026b4638fcb55
Manu84-bit <megaskatos@gmail.com>
2023-10-15 21:40:04 -0500

    product json file

--- main.go
+++ main.go
@@ -2,22 +2,38 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"os"
 
+	prod "github.com/Manu84-bit/gin-web-server/product"
 	"github.com/gin-gonic/gin"
 )
 
 
 func main(){	
 
-	p:= Product{
-		"MacBook Pro", 500.00, true,
+	p:= prod.Product{
+		Name: "MacBook Pro", Price: 500.00, Published: true,
 	}
 
-	jsonData, err := json.Marshal(p)
-	if err!= nil{
+	//Read json file and parse to a Product slice
+	var products []prod.Product
+	jsonFile, err1 := os.ReadFile("products.json")
+	if err1!= nil {
+		log.Fatal(err1)
+	}
+	json.Unmarshal(jsonFile, &products)
+
+
+	//Format Product slice to print as a string with json format
+	data, err := json.Marshal(products)
+		if err!= nil {
 		log.Fatal(err)
 	}
+	fmt.Println(string(data))
+
+
 
 	//crear router
 	router:= gin.Default()
@@ -26,9 +42,13 @@ func main(){
 	router.GET("/gin-greeting", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "Hello, stranger!",
-			"product": jsonData,
+			"product": p,
+			"products": products,
 		})
 	})
+
+
+	//
 
 	//run server
 	router.Run()
--- product/product.go
+++ product/product.go
@@ -1,7 +1,11 @@
 package product
 
 type Product struct {
-	Name      string
-	Price     float64
-	Published bool
+	Id           int     `json:"id"`
+	Name         string  `json:"nombre"`
+	Price        float64 `json:"precio"`
+	Stock        int     `json:"stock"`
+	Code         int     `json:"código"`
+	CreationDate string  `json:"fecha_creación"`
+	Published    bool    `json:"publicado"`
 }
